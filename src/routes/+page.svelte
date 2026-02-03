e688efcf90f83d3f1ca010b5967f76b1fd299d8e
jellman86 <179294116+Jellman86@users.noreply.github.com>
2026-02-03 21:42:16 +0000

    Differentiate bird/fish physics and zoom out slightly (v1.1.3)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
             </div>
 
             <p class="text-xl text-slate-300 font-light leading-relaxed">
-                Work in Progress (Build v1.1.0)
+                Work in Progress (Build v1.1.3)
             </p>
             
             <div class="flex flex-wrap gap-4 justify-center pt-8">
