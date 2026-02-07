66ae78ba1435e7073bbcbf3b16a7ec96ccc64ca9
jellman86 <179294116+Jellman86@users.noreply.github.com>
2026-02-07 11:22:01 +0000

    chore: update build hash to 04eca52

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,7 @@
     let showTrails = $state(false);
     
     let fps = $state(0);
-    const gitHash = 'f4783c9'; 
+    const gitHash = '04eca52'; 
 
     // Derived theme properties
     let ActiveComponent = $derived(themes[currentTheme].component);
